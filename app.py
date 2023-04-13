c830856bfa7675b1d78f4ab17a4aa3caae073656
Heni Bouafia <bouafia.heni@gmail.com>
2023-04-13 23:44:44 +0100

    update .env

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from my_portfolio import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    create_app().run(host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
